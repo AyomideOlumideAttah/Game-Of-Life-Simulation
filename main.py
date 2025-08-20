efbccda6965fcc795e0a3ccb772b06fac81616b6
AyomideOlumideAttah <ayomideattah369@gmail.com>
2025-08-19 21:58:12 -0500

    Merged online and local repo successfully

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from conway.template import run
 
-# def name_format(s: str):
-#     s_list = s.split(" ")
-#     if len(s_list) == 1:
-#         return s
-#     new_s = s_list[0]
-#     for word in s_list[1:]:
-#         new_s += "_" + word
-#     return new_s
-
 options = ["R-Pentomino", "Simple Oscillator", "Light Weight Space Ship", "Heavy Weight Space Ship", "Pulsar", "Glider"]
 print("Welcome to Conway's Game of Life - Variations! In this project, \nwe will vary the rules in the game.")
 
